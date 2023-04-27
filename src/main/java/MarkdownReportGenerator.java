054dcabe62451d20e3563d93f870fcdae9a0fc6c
Iman <ibhlool7@gmail.com>
2023-04-27 17:10:24 +0330

    :hammer: Add method level annotation

--- src/main/java/MarkdownReportGenerator.java
+++ src/main/java/MarkdownReportGenerator.java
@@ -1,21 +1,25 @@
 import japicmp.model.JApiChangeStatus;
 import japicmp.model.JApiClass;
+import japicmp.model.JApiMethod;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class MarkdownReportGenerator implements ReportGenerator {
+public class MarkdownReportGenerator extends ReportGenerator {
 
     private final Path reportFile;
-    private final HashMap<JApiChangeStatus, ArrayList<JApiClass>> map;
+    private final HashMap<JApiChangeStatus, ArrayList<JApiClass>> classMap;
+    private final HashMap<JApiChangeStatus, ArrayList<JApiMethod>> methodMap;
 
-    private final Function<String, String> addHeader = s -> String.format("### %s", s);
+    private final Function<String, String> addHeader = s -> String.format("## %s", s);
+    private final Function<String, String> addSubHeader = s -> String.format("#### %s", s);
     private final Function<String, String> addUnorderedList = s -> String.format("- %s", s);
     private final Function<JApiClass, String> classFormatter = apiClass -> new StringBuffer()
             .append("name : ")
@@ -24,23 +28,46 @@ public class MarkdownReportGenerator implements ReportGenerator {
             .append("changes : ")
             .append(apiClass.getCompatibilityChanges().isEmpty() ? " Nothing" : apiClass.getCompatibilityChanges().stream().map(Enum::toString).collect(Collectors.joining(" | ")))
             .toString();
+
+    private final Function<JApiMethod, String> methodFormatter = jApiMethod -> new StringBuffer()
+            .append("name : ")
+            .append(jApiMethod.getName())
+            .append(" - ")
+            .append("changes : ")
+            .append(jApiMethod.getCompatibilityChanges().isEmpty() ? " Nothing" : jApiMethod.getCompatibilityChanges().stream().map(Enum::toString).collect(Collectors.joining(" | ")))
+            .toString();
     private final Function<String, String> addHighLight = s -> String.format("> %s", s);
 
     public MarkdownReportGenerator() throws IOException {
         reportFile = Paths.get("report.md");
-        map = new HashMap<>();
+        classMap = new HashMap<>();
+        methodMap = new HashMap<>();
     }
 
     @Override
     public void record(JApiChangeStatus status, JApiClass apiClass) {
-        map.compute(status, (jApiChangeStatus, jApiClasses) -> {
+        classMap.compute(status, (jApiChangeStatus, jApiClasses) -> {
             if (jApiClasses == null) {
-                var a = new ArrayList<JApiClass>();
-                a.add(apiClass);
-                return a;
+                var newList = new ArrayList<JApiClass>();
+                newList.add(apiClass);
+                return newList;
             } else {
                 jApiClasses.add(apiClass);
                 return jApiClasses;
+            }
+        });
+    }
+
+    @Override
+    public void record(JApiChangeStatus status, JApiMethod jApiMethod) {
+        methodMap.compute(status, (jApiChangeStatus, jApiMethods) -> {
+            if (jApiMethods == null) {
+                var newList = new ArrayList<JApiMethod>();
+                newList.add(jApiMethod);
+                return newList;
+            } else {
+                jApiMethods.add(jApiMethod);
+                return jApiMethods;
             }
         });
     }
@@ -60,17 +87,32 @@ public class MarkdownReportGenerator implements ReportGenerator {
         assert writer != null;
 
         final var finalWriter = writer;
-        map.keySet().stream()
-                .forEach(status -> {
-                    finalWriter.println(addHeader.apply(status.name()));
-                    map.get(status).stream().forEach(apiClass -> {
-                        final var recordText = classFormatter.apply(apiClass);
-                        if (apiClass.getAnnotations().stream().anyMatch(jApiAnnotation -> jApiAnnotation.getFullyQualifiedName().contains("OnlyModified")))
-                            finalWriter.println(addUnorderedList.apply(addHighLight.apply(recordText)));
-                        else
-                            finalWriter.println(addUnorderedList.apply(recordText));
-                    });
+        Arrays.stream(JApiChangeStatus.values()).forEach(status -> {
+            finalWriter.println(addHeader.apply(status.name()));
+
+            if (enableClassReport) {
+                finalWriter.println(addSubHeader.apply("Classes"));
+                classMap.get(status).forEach(apiClass -> {
+                    final var recordText = classFormatter.apply(apiClass);
+                    if (apiClass.getAnnotations().stream().anyMatch(jApiAnnotation -> jApiAnnotation.getFullyQualifiedName().contains("OnlyModified")))
+                        finalWriter.println(addUnorderedList.apply(addHighLight.apply(recordText)));
+                    else
+                        finalWriter.println(addUnorderedList.apply(recordText));
                 });
+            }
+
+            if (enableMethodReport){
+                finalWriter.println(addSubHeader.apply("Methods"));
+                methodMap.get(status).forEach(jApiMethod -> {
+                    final var recordText = methodFormatter.apply(jApiMethod);
+                    if (jApiMethod.getAnnotations().stream().anyMatch(jApiAnnotation -> jApiAnnotation.getFullyQualifiedName().contains("OnlyModified")))
+                        finalWriter.println(addUnorderedList.apply(addHighLight.apply(recordText)));
+                    else
+                        finalWriter.println(addUnorderedList.apply(recordText));
+                });
+            }
+        });
+
         finalWriter.flush();
     }
 
--- src/main/java/ReportGenerator.java
+++ src/main/java/ReportGenerator.java
@@ -1,7 +1,22 @@
 import japicmp.model.JApiChangeStatus;
 import japicmp.model.JApiClass;
+import japicmp.model.JApiMethod;
 
-public interface ReportGenerator {
-    void record(JApiChangeStatus status, JApiClass apiClass);
-    void flush();
+public abstract class ReportGenerator {
+    protected boolean enableClassReport = Boolean.TRUE;
+    protected boolean enableMethodReport = Boolean.FALSE;
+
+    void setEnableClassReport(Boolean enable) {
+        enableClassReport = enable;
+    }
+
+    void setEnableMethodReport(Boolean enable) {
+        enableMethodReport = enable;
+    }
+
+    abstract void record(JApiChangeStatus status, JApiClass apiClass);
+
+    abstract void record(JApiChangeStatus status, JApiMethod jApiMethod);
+
+    abstract void flush();
 }
